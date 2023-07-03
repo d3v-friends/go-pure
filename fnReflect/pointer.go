3626b2dbb0b4883b78209690676f3155377e5593
CiaoLee <ciaolee87@gmail.com>
2023-07-03 10:53:52 +0900

    1. save

--- fnReflect/pointer.go
+++ fnReflect/pointer.go
@@ -1,5 +1,12 @@
 package fnReflect
 
+import "reflect"
+
 func ToPointer[T any](v T) *T {
 	return &v
+}
+
+func IsKind(v any, kind reflect.Kind) bool {
+	to := reflect.TypeOf(v)
+	return to.Kind() == kind
 }
