d102f00ce2fc06f40e710192e830e8047251bc66
CiaoLee <ciaolee87@gmail.com>
2023-07-08 14:45:57 +0900

    1. save

--- fnReflect/pointer.go
+++ fnReflect/pointer.go
@@ -10,3 +10,11 @@ func IsKind(v any, kind reflect.Kind) bool {
 	to := reflect.TypeOf(v)
 	return to.Kind() == kind
 }
+
+func UnboxPointer(v any) any {
+	to := reflect.TypeOf(v)
+	if to.Kind() == reflect.Pointer {
+		return reflect.ValueOf(v).Elem().Interface()
+	}
+	return v
+}
