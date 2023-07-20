03d1f21a2c45d1ac4ae00c319bb6557e185e92b7
CiaoLee <ciaolee87@gmail.com>
2023-07-20 17:47:13 +0900

    1. update fnPanic

--- fnPanic/on.go
+++ fnPanic/on.go
@@ -6,7 +6,14 @@ func On(err error) {
 	}
 }
 
-func OnValue[T any](value *T, err error) *T {
+func OnPointer[T any](value *T, err error) *T {
+	if err != nil {
+		panic(err)
+	}
+	return value
+}
+
+func OnValue[T any](value T, err error) T {
 	if err != nil {
 		panic(err)
 	}
