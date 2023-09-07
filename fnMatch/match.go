693d10665e0f7b2868a216cd7d3e01fa57e1eaa4
CiaoLee <ciaolee87@gmail.com>
2023-09-07 21:52:08 +0900

    1. add fnMatch.Get()

--- fnMatch/match.go
+++ fnMatch/match.go
@@ -1,5 +1,7 @@
 package fnMatch
 
+import "fmt"
+
 type (
 	FnMatcher[T any] func(v T) bool
 )
@@ -24,4 +26,23 @@ func Contain[T OriginType](ls []T, v T) bool {
 		}
 	}
 	return false
+}
+
+func Get[T any](ls []T, matcher FnMatcher[T]) (res T, err error) {
+	for _, item := range ls {
+		if matcher(item) {
+			res = item
+			return
+		}
+	}
+	err = fmt.Errorf("not found element")
+	return
+}
+
+func GetP[T any](ls []T, matcher FnMatcher[T]) (res T) {
+	var err error
+	if res, err = Get(ls, matcher); err != nil {
+		panic(err)
+	}
+	return
 }
