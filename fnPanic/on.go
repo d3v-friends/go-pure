041cc9c6e6335e9822c9aacc6ccb7831fa1b4aaa
CiaoLee (Win) <ciao.junhyeok@gmail.com>
2023-10-11 17:27:31 +0900

    1. add logger in fnPanic.* (1)

--- fnPanic/on.go
+++ fnPanic/on.go
@@ -1,44 +1,76 @@
 package fnPanic
 
-import "github.com/d3v-friends/go-pure/fnLogger"
+import (
+	"fmt"
+	"github.com/d3v-friends/go-pure/fnLogger"
+	"log"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
 
 func On(err error) {
 	if err != nil {
-		var logger = fnLogger.NewDefaultLogger()
-		logger.Fatal("err: %s", err.Error())
-		panic(err)
+		panicLogger("%s", err.Error())
+		panic("")
 	}
 }
 
 func OnPointer[T any](value *T, err error) *T {
 	if err != nil {
-		var logger = fnLogger.NewDefaultLogger()
-		logger.Fatal("err: %s", err.Error())
-		panic(err)
+		panicLogger("%s", err.Error())
+		panic("")
 	}
 	return value
 }
 
 func OnValue[T any](value T, err error) T {
 	if err != nil {
-		var logger = fnLogger.NewDefaultLogger()
-		logger.Fatal("err: %s", err.Error())
-		panic(err)
+		panicLogger("%s", err.Error())
+		panic("")
 	}
 	return value
 }
 
 func Get[T any](value T, err error) T {
 	if err != nil {
-		var logger = fnLogger.NewDefaultLogger()
-		logger.Fatal("err: %s", err.Error())
+		panicLogger("%s", err.Error())
+		panic("")
 	}
 	return value
 }
 
 func IsTrue(v bool, err error) {
 	if !v {
-		var logger = fnLogger.NewDefaultLogger()
-		logger.Fatal("err: %s", err.Error())
+		panicLogger("%s", err.Error())
+		panic("")
 	}
+}
+
+func panicLogger(format string, args ...any) {
+	var depth = runtime.NumCgoCall()
+
+	var maxSkip = int64(5)
+	if depth < 5 {
+		maxSkip = depth
+	}
+
+	var v = fnLogger.Fields{}
+	for i := int64(0); i < maxSkip; i++ {
+		var pc, file, line, ok = runtime.Caller(int(i))
+		if !ok {
+			file = "not_found"
+			line = 0
+		}
+
+		var fnName = "not_found_fn()"
+		var fn = runtime.FuncForPC(pc)
+		if fn != nil {
+			fnName = fmt.Sprintf("%s()", strings.TrimLeft(filepath.Ext(fn.Name()), "."))
+		}
+
+		v[fmt.Sprintf("callstack_%d", i)] = fmt.Sprintf("%s:%d %s", filepath.Base(file), line, fnName)
+	}
+
+	log.Print(v.ToJson())
 }
