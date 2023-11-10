99ddf86c694956b0cfac4beece579a3e4c9625c8
xcode_windesk <xcode@stdlib.in>
2023-11-10 17:00:13 +0900

    1. save

--- tag.sh
+++ tag.sh
@@ -1,4 +1,32 @@
-TAG=$1
+#!/bin/bash
 
-git tag "$TAG"
-git push origin "$TAG"
+VERS=$1
+
+git tag "$VERS"
+git push --tag
+
+
+# PATCH=$1
+# MINOR=$2
+# MAJOR=$3
+# FILE=version
+#
+# if [ ! -f $FILE ]; then
+#     echo "not found version"
+#     echo "0.0.0" > $FILE
+# fi
+#
+# VERS=$(echo "$(cat $FILE)" | tr "." "\n")
+# echo "length: $(!$VERS[@])"
+#
+# if [[ $PATCH -eq "true" ]]; then
+#     echo "patch up: patch=$VERS[2]"
+# fi
+#
+# if [[ $MINOR -eq "true" ]]; then
+#     echo "minor up: minor=$VERS[1]"
+# fi
+#
+# if [[ $MAJOR -eq "true" ]]; then
+#     echo "minor up: major=$VERS[0]"
+# fi
